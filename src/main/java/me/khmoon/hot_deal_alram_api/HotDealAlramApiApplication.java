4ffdc699a7316f9b3cdb0a2e3e02c5091df45a10
kwanghyunMoon <mgh3326@naver.com>
2019-12-18 21:22:23 +0900

    Initial commit

---
+++ src/main/java/me/khmoon/hot_deal_alram_api/HotDealAlramApiApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.hot_deal_alram_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDealAlramApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotDealAlramApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/khmoon/hot_deal_alram_api/HotDealAlramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khmoon.hot_deal_alram_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDealAlramApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
