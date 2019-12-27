22938d109c9ce62a621427a1debaf28a331f18d0
robin <mgh3326@naver.com>
2019-12-27 22:53:44 +0900

    add repository

---
+++ src/main/java/me/khmoon/hot_deal_alram_api/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package me.khmoon.hot_deal_alram_api.repository;
+
+import me.khmoon.hot_deal_alram_api.domain.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
