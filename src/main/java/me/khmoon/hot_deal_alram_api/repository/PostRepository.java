b5a97737207437f49bd0ca31b53b47b5b5c412de
robin <mgh3326@naver.com>
2019-12-28 14:22:02 +0900

    fix typos

--- src/main/java/me/khmoon/hot_deal_alram_api/repository/PostRepository.java
+++ src/main/java/me/khmoon/hot_deal_alram_api/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package me.khmoon.hot_deal_alram_api.repository;
 
-import me.khmoon.hot_deal_alram_api.domain.board.Board;
+import me.khmoon.hot_deal_alram_api.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Board, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
