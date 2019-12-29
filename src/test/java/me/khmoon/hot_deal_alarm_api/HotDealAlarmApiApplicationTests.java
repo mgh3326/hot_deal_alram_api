5bb4c9f3beaea99d7fbb746ef35d68bf687ea2ad
robin <mgh3326@naver.com>
2019-12-29 12:49:38 +0900

    remove test main function

--- src/test/java/me/khmoon/hot_deal_alarm_api/HotDealAlarmApiApplicationTests.java
+++ src/test/java/me/khmoon/hot_deal_alarm_api/HotDealAlarmApiApplicationTests.java
@@ -3,19 +3,10 @@ package me.khmoon.hot_deal_alarm_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class HotDealAlarmApiApplicationTests {
 
   @Test
   void contextLoads() {
-  }
-
-  @Test
-  public void main() {
-
-    HotDealAlarmApiApplication.main(new String[]{});
-    assertTrue(true, "main test");
   }
 }
