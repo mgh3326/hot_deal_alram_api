718b53bc17db4b31c3b2bbc0c98b573e3fcbfa51
kwanghyunMoon <mgh3326@naver.com>
2020-01-24 22:47:39 +0900

    remove embedded redis

--- src/main/java/me/khmoon/hot_deal_alarm_api/config/RedisRepositoryConfig.java
+++ src/main/java/me/khmoon/hot_deal_alarm_api/config/RedisRepositoryConfig.java
@@ -1,9 +1,10 @@
-package me.khmoon.hot_deal_alarm_api.propertiy;
+package me.khmoon.hot_deal_alarm_api.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -13,13 +14,18 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisRepositoryConfig {
   @Value("${spring.redis.host}")
   private String redisHost;
-
   @Value("${spring.redis.port}")
   private int redisPort;
+  @Value("${spring.redis.password}")
+  private String redisPwd;
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(redisHost, redisPort);
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(redisHost);
+    redisStandaloneConfiguration.setPort(redisPort);
+    redisStandaloneConfiguration.setPassword(redisPwd); //redis에 비밀번호가 설정 되어 있는 경우 설정해주면 됩니다.
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 
   @Bean
